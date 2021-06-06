fbd4669a59a7b7ae1ca47940cd5d3c8d7e3062ea
vagrant <vagrant@localhost.localdomain>
2021-06-06 03:32:25 -0700

    Added tests for both centos and ubuntu

--- cookbooks/apache/spec/unit/recipes/default_spec.rb
+++ cookbooks/apache/spec/unit/recipes/default_spec.rb
@@ -7,9 +7,9 @@
 require 'spec_helper'
 
 describe 'apache::default' do
-  context 'When all attributes are default, on an unspecified platform' do
+  context 'when run on centos' do
     let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new
+      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.2.1511')
       runner.converge(described_recipe)
     end
 
@@ -17,8 +17,50 @@ describe 'apache::default' do
       expect(chef_run).to install_package('httpd')
     end
 
+    it 'creates a default html file' do
+      expect(chef_run).to create_template('/var/www/html/index.html')
+    end
+
+    it 'starts the service' do
+      expect(chef_run).to start_service('httpd')
+    end
+
+    it 'enables the service' do
+      expect(chef_run).to enable_service('httpd')
+    end
+
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
   end
 end
+
+describe 'apache::default' do
+  context 'when run on ubuntu' do
+    let(:chef_run) do
+      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
+      runner.converge(described_recipe)
+    end
+
+    it 'installs the correct package' do
+      expect(chef_run).to install_package('apache2')
+    end
+
+    it 'creates a default html file' do
+      expect(chef_run).to create_template('/var/www/html/index.html')
+    end
+
+    it 'starts the service' do
+      expect(chef_run).to start_service('apache2')
+    end
+
+    it 'enables the service' do
+      expect(chef_run).to enable_service('apache2')
+    end
+
+    it 'converges successfully' do
+      expect { chef_run }.to_not raise_error
+    end
+  end
+end
+
